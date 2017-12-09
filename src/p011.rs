5ac3b35648f5eb88e2b05d9ddf9fbf380e0df2dd
John-John Tedro <udoprog@tedro.se>
2017-12-09 14:37:57 +0100

    Add: p011

---
+++ src/p011.rs
@@ -0,0 +1,64 @@
+static INPUT: &str = include_str!("p011_grid.txt");
+
+use std::io::{BufRead, BufReader, Cursor};
+use std::iter;
+
+fn run(count: usize) -> u64 {
+    let mut input: Vec<Vec<u64>> = Vec::new();
+
+    for line in BufReader::new(Cursor::new(INPUT)).lines() {
+        let line = line.expect("bad line");
+        let line = line.trim();
+
+        input.push(
+            line.split_whitespace()
+                .map(|d| d.parse::<u64>())
+                .collect::<Result<Vec<_>, _>>()
+                .expect("a bad line"),
+        );
+    }
+
+    let width = input.iter().map(|line| line.len()).min().expect(
+        "bad width",
+    );
+
+    let mut products = Vec::new();
+
+    for line in &input {
+        // horizontally
+        for i in 0..(line.len() - (count - 1)) {
+            products.push(line[i..(i + count)].iter().fold(1u64, |r, &v| r * v))
+        }
+    }
+
+    // vertically
+    for x in 0..width {
+        for y in 0..(input.len() - (count - 1)) {
+            products.push(product(&input, iter::repeat(x).take(4).zip(y..=y+3)));
+        }
+    }
+
+    // diagonals
+    for x in 0..(width - (count - 1)) {
+        for y in 0..(input.len() - (count - 1)) {
+            products.push(product(&input, (x..=x+3).zip(y..=y+3)));
+            products.push(product(&input, (x..=x+3).rev().zip(y..=y+3)));
+        }
+    }
+
+    products.sort();
+    return products[products.len() - 1];
+
+    fn product<I>(input: &Vec<Vec<u64>>, range: I) -> u64
+    where
+        I: IntoIterator<Item = (usize, usize)>,
+    {
+        range.into_iter().map(|(x, y)| input[y][x]).fold(1u64, |p, v| p * v)
+    }
+}
+
+problem!{
+    tests => [
+        q => (70600674, run(4)),
+    ];
+}
