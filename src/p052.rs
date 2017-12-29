36aa2309026b8bb53cc8708c67bdaa4d8438b113
John-John Tedro <udoprog@tedro.se>
2017-12-29 20:48:23 +0100

    Add: p052

---
+++ src/p052.rs
@@ -0,0 +1,33 @@
+/// Keywords: none
+
+use p032::digits;
+
+fn run() -> u64 {
+    for n in 1.. {
+        let mut a: Vec<u8> = digits(n).collect();
+        let mut b: Vec<u8> = digits(n * 2).collect();
+        let mut c: Vec<u8> = digits(n * 3).collect();
+        let mut d: Vec<u8> = digits(n * 4).collect();
+        let mut e: Vec<u8> = digits(n * 5).collect();
+        let mut f: Vec<u8> = digits(n * 6).collect();
+
+        a.sort();
+        b.sort();
+        c.sort();
+        d.sort();
+        e.sort();
+        f.sort();
+
+        if a == b && a == c && a == d && a == e && a == f {
+            return n;
+        }
+    }
+
+    panic!("no solution found");
+}
+
+problem!{
+    tests => [
+        q => {run(), "ebd72b510911af3e254a030cd891cb804e1902189eee7a0f6199472eb5e4dba2"},
+    ];
+}
